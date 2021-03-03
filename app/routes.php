3109fcf921620010607353f50765eac3f1627df3
ramunas <ramunas.danupas@gmail.com>
2021-03-03 16:19:18 +0200

    quick fix

--- app/routes.php
+++ app/routes.php
@@ -3,4 +3,3 @@
 $router->get('', 'HomeController@index');
 $router->get('articles', 'ArticlesController@index');
 $router->post('articles', 'ArticlesController@store');
-$router->post('content', 'ArticlesController@storeContent');
